1ffba8ea24106fdc7647e6deb769cebabd521c9e
JaeEun Kim <140696764+babyshrimps@users.noreply.github.com>
2025-06-10 14:12:37 +0900

    [기능추가] 날짜 입력 및 날짜순 정렬 명령 추가

--- OSS_Budget/budget.py
+++ OSS_Budget/budget.py
@@ -5,9 +5,16 @@ class Budget:
     def __init__(self):
         self.expenses = []
 
-    def add_expense(self, category, description, amount):
-        today = datetime.date.today().isoformat()
-        expense = Expense(today, category, description, amount)
+    def add_expense(self, date, category, description, amount):
+	try:
+            if date == 't':
+                ex_date = datetime.date.today()
+            else:
+                ex_date = datetime.datetime.strptime(date, '%Y.%m.%d').date()
+        except ValueError:
+            print("날짜 형식이 올바르지 않음. 'YYYY.MM.DD' 또는 't'로 입력.\n")
+            return
+        expense = Expense(ex_date, category, description, amount)
         self.expenses.append(expense)
         print("지출이 추가되었습니다.\n")
 
@@ -15,13 +22,13 @@ class Budget:
         if not self.expenses:
             print("지출 내역이 없습니다.\n")
             return
+	sorted_ex = sorted(self.expenses, key=lambda e: e.date)
         print("\n[지출 목록]")
-        for idx, e in enumerate(self.expenses, 1):
+        for idx, e in enumerate(sorted_ex, 1):
             print(f"{idx}. {e}")
         print()
 
     def total_spent(self):
         total = sum(e.amount for e in self.expenses)
         print(f"총 지출: {total}원\n")
-
 
