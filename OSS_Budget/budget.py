47d4318331f7e18c69d9b00d13b93363ab573a6e
JaeEun Kim <140696764+babyshrimps@users.noreply.github.com>
2025-06-10 14:21:58 +0900

    [기능추가] 지출 수정 및 공지 추가

--- OSS_Budget/budget.py
+++ OSS_Budget/budget.py
@@ -2,8 +2,8 @@ import datetime
 from expense import Expense
 
 class Budget:
-    def __init__(self):
-        self.expenses = []
+	def __init__(self):
+		self.expenses = []
 
     def add_expense(self, category, description, amount):
         today = datetime.date.today().isoformat()
@@ -23,5 +23,22 @@ class Budget:
     def total_spent(self):
         total = sum(e.amount for e in self.expenses)
         print(f"총 지출: {total}원\n")
+        
+    def edit_expenses(self):
+	    self.list_expenses() 
+	    if not self.expenses:
+		    return
 
-
+	    print("수정할 지출 내역을 선택하시오 : ")
+	    choose_edit = int(input())
+	    
+	    if choose_edit < 1 or choose_edit > len(self.expenses):
+		    print("잘못입력. 다시 시도하세요.\n")
+		    return
+		    
+	    else :
+		    print("수정할 금액을 입력 : ")
+		    new_amount = int(input())
+		    self.expenses[choose_edit - 1].amount = new_amount
+		    print(f"{choose_edit}번 지출 내역의 금액이 {new_amount}원으로 수정.\n")
+		    self.total_spent()
